d48cc7d9d840af4aaf7096b46eb4228518451d64
Chéridanh <divintsiela@gmail.com>
2025-04-16 22:54:13 +0200

    adding handle for user

---
+++ src/main/java/com/example/bachelorprojectjava/handlers/UserHandler.java
@@ -0,0 +1,16 @@
+package com.example.bachelorprojectjava.handlers;
+
+import com.example.bachelorprojectjava.models.User;
+import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+@RepositoryEventHandler
+public class UserHandler {
+
+    @HandleBeforeCreate
+    public void handleUserBeforeCreate(User user) {
+        user.setEnabled(true);
+    }
+}
