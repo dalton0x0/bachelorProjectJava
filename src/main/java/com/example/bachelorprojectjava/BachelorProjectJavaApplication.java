cc5e3cd59eabb73a94f06429ae8f578199646f99
Chéridanh <divintsiela@gmail.com>
2025-04-16 17:29:12 +0200

    first commit after create project

---
+++ src/main/java/com/example/bachelorprojectjava/BachelorProjectJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.bachelorprojectjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BachelorProjectJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BachelorProjectJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bachelorprojectjava/BachelorProjectJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bachelorprojectjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BachelorProjectJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
