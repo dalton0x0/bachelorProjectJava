ba879baf400ccd428c7b1c453c47603525178814
Chéridanh <divintsiela@gmail.com>
2025-04-16 18:54:44 +0200

    add swagger-ui & dot env dependency and configure .env file

--- src/main/java/com/example/bachelorprojectjava/BachelorProjectJavaApplication.java
+++ src/main/java/com/example/bachelorprojectjava/BachelorProjectJavaApplication.java
@@ -1,5 +1,6 @@
 package com.example.bachelorprojectjava;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BachelorProjectJavaApplication {
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_CONNECTION", dotenv.get("DB_CONNECTION"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("MONGODB_URI", dotenv.get("MONGODB_URI"));
+        System.setProperty("MONGODB_DATABASE", dotenv.get("MONGODB_DATABASE"));
+
         SpringApplication.run(BachelorProjectJavaApplication.class, args);
     }
 
