9a4582022b07213a2746088647b6644c061361ed
Chéridanh <divintsiela@gmail.com>
2025-04-16 18:00:20 +0200

    add user entity, repository and configure application.properties file

---
+++ src/main/java/com/example/bachelorprojectjava/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.bachelorprojectjava.repositories;
+
+import com.example.bachelorprojectjava.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
