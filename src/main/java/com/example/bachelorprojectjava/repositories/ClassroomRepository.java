deed298b06e69ca4c9d61396013cc94b86428f4b
Chéridanh <divintsiela@gmail.com>
2025-05-04 10:07:19 +0200

    add method on validation to create classroom

--- src/main/java/com/example/bachelorprojectjava/repositories/ClassroomRepository.java
+++ src/main/java/com/example/bachelorprojectjava/repositories/ClassroomRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ClassroomRepository extends JpaRepository<Classroom, Long> {
+    boolean existsByName(String name);
     List<Classroom> findByNameContaining(String name);
 }
--- src/main/java/com/example/bachelorprojectjava/validators/ClassroomValidator.java
+++ src/main/java/com/example/bachelorprojectjava/validators/ClassroomValidator.java
@@ -1,12 +1,17 @@
 package com.example.bachelorprojectjava.validators;
 
 import com.example.bachelorprojectjava.models.Classroom;
+import com.example.bachelorprojectjava.repositories.ClassroomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 @Component
 public class ClassroomValidator implements Validator {
+
+    @Autowired
+    private ClassroomRepository classroomRepository;
 
     @Override
     public boolean supports(Class<?> clazz) {
@@ -20,6 +25,10 @@ public class ClassroomValidator implements Validator {
 
         if (classroom.getName() == null || classroom.getName().trim().isEmpty()) {
             errors.rejectValue("name", "name.required", "Name is required");
+        }
+
+        if (classroomRepository.existsByName(classroom.getName())) {
+            errors.rejectValue("name", "name.exists", "Name already exists");
         }
     }
 }
